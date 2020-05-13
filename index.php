b37d9fc265c9db1a9985e52bd8e5ddd26d4ff1fd
tbasalla <tbasalla@gmail.com>
2020-05-13 11:52:29 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php $page_title = 'Synthelize - Big Data Consulting - Pittsburgh'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/header.php";?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/snippets/" . "main_index.php";?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/footer.php";?>
