6fc97e8f9b67470dd4384b853c12faa2441d8efa
tbasalla <tbasalla@gmail.com>
2020-05-17 10:48:06 -0400

    automated commit

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php $page_title = 'Synthelize - Big Data Consulting - Pittsburgh'; ?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/header.php";?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/snippets/" . "main_index.php";?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/contactus.php";?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/footer.php";?>
