bf1d1768d8f4a4b2e2d32a8d31167d3afd9f85ca
tbasalla <tbasalla@gmail.com>
2020-05-17 10:09:57 -0400

    made more changes

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
 <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo mr-auto"><a href="#header" class="scrollto">Anyar</a></h1>
+      <h1 class="logo mr-auto"><a href="#header" class="scrollto">Synthelize</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="#header" class="logo mr-auto scrollto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
@@ -40,30 +40,11 @@
         <ul>
           <li class="active"><a href="#header">Home</a></li>
           <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#team">Team</a></li>
-          <li><a href="#pricing">Pricing</a></li>
+          <li><a href="#services">Our Process</a></li>
+          <li><a href="#portfolio">BI Strategy</a></li>
+          <li><a href="#team">Data Lakes</a></li>
           <li><a href="blog.html">Blog</a></li>
-          <li class="drop-down"><a href="">Drop Down</a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="drop-down"><a href="#">Deep Drop Down</a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
           <li><a href="#contact">Contact</a></li>
-
         </ul>
       </nav><!-- .nav-menu -->
 
