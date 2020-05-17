30f210e90f5332fd372302a169180f8467e812cf
tbasalla <tbasalla@gmail.com>
2020-05-17 10:51:24 -0400

    automated commit

--- footer.php
+++ footer.php
@@ -67,25 +67,13 @@
 <!-- ======= Footer ======= -->
 <footer id="footer">
 
-  <div class="footer-newsletter">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6">
-          <h4>Our Newsletter</h4>
-          <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-        </div>
-        <div class="col-lg-6">
-          <form action="" method="post">
-            <input type="email" name="email"><input type="submit" value="Subscribe">
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
-
   <div class="footer-top">
     <div class="container">
       <div class="row">
+        <div class="col-lg-3 col-md-6 footer-info">
+            <h3>About Synthelize</h3>
+            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+          </div>
 
         <div class="col-lg-3 col-md-6 footer-links">
           <h4>Useful Links</h4>
@@ -121,17 +109,6 @@
 
         </div>
 
-        <div class="col-lg-3 col-md-6 footer-info">
-          <h3>About Anyar</h3>
-          <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
-          <div class="social-links mt-3">
-            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-            <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-          </div>
-        </div>
 
       </div>
     </div>
