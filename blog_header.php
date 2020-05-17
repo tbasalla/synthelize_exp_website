1f2760782658775da5d6fad8ddcb8bcf5586a276
tbasalla <tbasalla@gmail.com>
2020-05-17 16:14:02 -0400

    automated commit

---
+++ blog_header.php
@@ -0,0 +1,11 @@
+<main id="main">
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="../">Home</a></li>
+          <li><a href="../blog">Blog</a></li>
+        </ol>
+      </div>
+    </section><!-- End Breadcrumbs -->
--- data-lake-azure/index.php
+++ data-lake-azure/index.php
@@ -1,5 +1,6 @@
 <?php $page_title = 'Data Lake Azure - Synthelize'; ?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/hit_tracker.php";?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/blog_header.php";?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/header.php";?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/snippets/" . "data_lake_azure.php";?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/footer.php";?>
