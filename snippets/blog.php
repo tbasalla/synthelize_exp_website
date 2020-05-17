97c47eefc355378fba74fe6251f9c36815d3915b
tbasalla <tbasalla@gmail.com>
2020-05-17 15:57:34 -0400

    automated commit

--- snippets/blog.php
+++ snippets/blog.php
@@ -27,22 +27,12 @@
           </div>
 
           <h2 class="entry-title">
-            <a href="/data-lake-azure">Azure Data Lake</a>
+            <a href="/data-lake-azure">Introducing Synthelize</a>
           </h2>
-
-          <div class="entry-meta">
-            <ul>
-              <li class="d-flex align-items-center"><i class="icofont-user"></i> <a href="blog-single.html">John Doe</a></li>
-              <li class="d-flex align-items-center"><i class="icofont-wall-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a></li>
-            </ul>
-          </div>
-
           <div class="entry-content">
-            <p>
-              Similique neque nam consequuntur ad non maxime aliquam quas. Quibusdam animi praesentium. Aliquam et laboriosam eius aut nostrum quidem aliquid dicta zena prista maraeda talan mas indera.
-            </p>
+            <p>Who we are, what we do, and why you should work with us for all your business data needs.</p>
             <div class="read-more">
-              <a href="blog-single.html">Read More</a>
+              <a href="/introducing-synthelize">Read More</a>
             </div>
           </div>
 
@@ -52,27 +42,93 @@
       <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
         <article class="entry">
 
-          <div class="entry-img">
-            <img src="../assets/img/blog-2.jpg" alt="" class="img-fluid">
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
           </div>
 
           <h2 class="entry-title">
-            <a href="blog-single.html">Nisi magni odit consequatur autem nulla dolorem</a>
+            <a href="/data-lake-azure">Do You Have a Data Strategy?</a>
           </h2>
-
-          <div class="entry-meta">
-            <ul>
-              <li class="d-flex align-items-center"><i class="icofont-user"></i> <a href="blog-single.html">John Doe</a></li>
-              <li class="d-flex align-items-center"><i class="icofont-wall-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a></li>
-            </ul>
+          <div class="entry-content">
+            <p>Learn the difference between having a data system and a data strategy.</p>
+            <div class="read-more">
+              <a href="/do-you-have-data-strategy/">Read More</a>
+            </div>
           </div>
 
+        </article><!-- End blog entry -->
+      </div>
+      <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <article class="entry">
+
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
+          </div>
+
+          <h2 class="entry-title">
+            <a href="/data-lake-azure">Our Five Step Process</a>
+          </h2>
           <div class="entry-content">
-            <p>
-              Ad impedit qui officiis est in non aliquid veniam laborum. Id ipsum qui aut. Sit aliquam et quia molestias laboriosam. Tempora nam odit omnis eum corrupti qui aliquid excepturi molestiae. Facilis et sint quos sed voluptas. Maxime sed tempore enim omnis non alias.
-            </p>
+            <p>The process we use to plan and build data systems, architectures, and visualization dashboards.</p>
             <div class="read-more">
-              <a href="blog-single.html">Read More</a>
+              <a href="/our-process/">Read More</a>
+            </div>
+          </div>
+
+        </article><!-- End blog entry -->
+      </div>
+      <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <article class="entry">
+
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
+          </div>
+
+          <h2 class="entry-title">
+            <a href="/data-lake-azure">What is a Data Lake?</a>
+          </h2>
+          <div class="entry-content">
+            <p>Introduction to data lakes and techniques for handling big data.</p>
+            <div class="read-more">
+              <a href="/what-is-a-data-lake/">Read More</a>
+            </div>
+          </div>
+
+        </article><!-- End blog entry -->
+      </div>
+      <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <article class="entry">
+
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
+          </div>
+
+          <h2 class="entry-title">
+            <a href="/data-lake-azure">Building a Data Lake</a>
+          </h2>
+          <div class="entry-content">
+            <p>Learn the basics of building a data lake.</p>
+            <div class="read-more">
+              <a href="/building-a-data-lake">Read More</a>
+            </div>
+          </div>
+
+        </article><!-- End blog entry -->
+      </div>
+      <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <article class="entry">
+
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
+          </div>
+
+          <h2 class="entry-title">
+            <a href="/data-lake-azure">Data Lake Solutions</a>
+          </h2>
+          <div class="entry-content">
+            <p>An exploration of the major cloud vendors and their data lake solutions.</p>
+            <div class="read-more">
+              <a href="/data-lake-cloud-options/">Read More</a>
             </div>
           </div>
 
@@ -82,27 +138,17 @@
       <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
         <article class="entry">
 
-          <div class="entry-img">
-            <img src="../assets/img/blog-3.jpg" alt="" class="img-fluid">
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
           </div>
 
           <h2 class="entry-title">
-            <a href="blog-single.html">Possimus soluta ut id suscipit ea ut. In quo quia et soluta libero sit sint.</a>
+            <a href="/data-lake-azure">Data Lake vs. Data Warehouse</a>
           </h2>
-
-          <div class="entry-meta">
-            <ul>
-              <li class="d-flex align-items-center"><i class="icofont-user"></i> <a href="blog-single.html">John Doe</a></li>
-              <li class="d-flex align-items-center"><i class="icofont-wall-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a></li>
-            </ul>
-          </div>
-
           <div class="entry-content">
-            <p>
-              Aut iste neque ut illum qui perspiciatis similique recusandae non. Fugit autem dolorem labore omnis et. Eum temporibus fugiat voluptate enim tenetur sunt omnis tara kero pakla metaruna nedore stan.
-            </p>
+            <p>An examination of the roles of data lakes and data warehouses. </p>
             <div class="read-more">
-              <a href="blog-single.html">Read More</a>
+              <a href="/data-lake-vs-data-warehouse/">Read More</a>
             </div>
           </div>
 
@@ -112,27 +158,17 @@
       <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
         <article class="entry">
 
-          <div class="entry-img">
-            <img src="../assets/img/blog-4.jpg" alt="" class="img-fluid">
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
           </div>
 
           <h2 class="entry-title">
-            <a href="blog-single.html">Non rem rerum nam cum quo minus. Dolor distinctio deleniti explicabo eius exercitationem.</a>
+            <a href="/data-lake-azure">Azure Data Lakes</a>
           </h2>
-
-          <div class="entry-meta">
-            <ul>
-              <li class="d-flex align-items-center"><i class="icofont-user"></i> <a href="blog-single.html">John Doe</a></li>
-              <li class="d-flex align-items-center"><i class="icofont-wall-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a></li>
-            </ul>
-          </div>
-
           <div class="entry-content">
-            <p>
-              Aspernatur rerum perferendis et sint. Voluptates cupiditate voluptas atque quae. Rem veritatis rerum enim et autem. Saepe atque cum eligendi eaque iste omnis a qui.
-            </p>
+            <p>A closer look at Microsoft Azure data lake technology. </p>
             <div class="read-more">
-              <a href="blog-single.html">Read More</a>
+              <a href="/data-lake-azure">Read More</a>
             </div>
           </div>
 
@@ -142,74 +178,22 @@
       <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
         <article class="entry">
 
-          <div class="entry-img">
-            <img src="../assets/img/blog-5.jpg" alt="" class="img-fluid">
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
           </div>
 
           <h2 class="entry-title">
-            <a href="blog-single.html">Blanditiis dignissimos deleniti. Rerum iste et.</a>
+            <a href="/data-lake-azure">Keys to BI Success</a>
           </h2>
-
-          <div class="entry-meta">
-            <ul>
-              <li class="d-flex align-items-center"><i class="icofont-user"></i> <a href="blog-single.html">John Doe</a></li>
-              <li class="d-flex align-items-center"><i class="icofont-wall-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a></li>
-            </ul>
-          </div>
-
           <div class="entry-content">
-            <p>
-              Quidem et eum explicabo quia illo numquam nostrum corrupti provident. Quia aspernatur et et facere. Quisquam maiores natus nihil incidunt ipsum est optio eum maxime. Dignissimos vitae explicabo. Corrupti esse sed a a. Laborum optio reprehenderit quia dena per.
-            </p>
+            <p>Five key steps to ensure the success of your next business intelligence project.</p>
             <div class="read-more">
-              <a href="blog-single.html">Read More</a>
+              <a href="/keys-to-bi-success/">Read More</a>
             </div>
           </div>
 
         </article><!-- End blog entry -->
       </div>
-
-      <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
-        <article class="entry">
-
-          <div class="entry-img">
-            <img src="../assets/img/blog-6.jpg" alt="" class="img-fluid">
-          </div>
-
-          <h2 class="entry-title">
-            <a href="blog-single.html">Debitis cupiditate saepe ex quam aut id. Consequatur dignissimos et id id.</a>
-          </h2>
-
-          <div class="entry-meta">
-            <ul>
-              <li class="d-flex align-items-center"><i class="icofont-user"></i> <a href="blog-single.html">John Doe</a></li>
-              <li class="d-flex align-items-center"><i class="icofont-wall-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a></li>
-            </ul>
-          </div>
-
-          <div class="entry-content">
-            <p>
-              Modi dolor et placeat ut iure ad. Qui perferendis fugit quo et cumque facilis et debitis rerum. Repellendus animi qui eos. Unde perferendis et tempora Ratione porro omnis magn delata sera marto ned.
-            </p>
-            <div class="read-more">
-              <a href="blog-single.html">Read More</a>
-            </div>
-          </div>
-
-        </article><!-- End blog entry -->
-      </div>
-
     </div>
-
-    <div class="blog-pagination" data-aos="fade-up">
-      <ul class="justify-content-center">
-        <li class="disabled"><i class="icofont-rounded-left"></i></li>
-        <li><a href="#">1</a></li>
-        <li class="active"><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="#"><i class="icofont-rounded-right"></i></a></li>
-      </ul>
-    </div>
-
   </div>
 </section><!-- End Blog Section -->
