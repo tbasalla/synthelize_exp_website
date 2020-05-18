f0667d75f044bf516ec915b05a4e95b4c1399d85
tbasalla <tbasalla@gmail.com>
2020-05-18 09:18:26 -0400

    automated commit

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
 <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo mr-auto"><a href="../" class="scrollto">Synthelize</a></h1>
+      <!-- <h1 class="logo mr-auto"><a href="../" class="scrollto">Synthelize</a></h1> -->
       <!-- Uncomment below if you prefer to use an image logo -->
       <a href="#header" class="logo mr-auto scrollto"><img src="../assets/img/logo-icon.svg" alt="" class="img-fluid"></a>
 
