5d912d0011e0307217f3fc0d8f643296116d6cd0
tbasalla <tbasalla@gmail.com>
2020-05-19 09:54:53 -0400

    automated commit

--- index.php
+++ index.php
@@ -1,5 +1,400 @@
-<?php $page_title = 'Synthelize - Big Data Consulting - Pittsburgh'; ?>
-<?php include $_SERVER['DOCUMENT_ROOT'] . "/header.php";?>
-<?php include $_SERVER['DOCUMENT_ROOT'] . "/snippets/" . "main_index.php";?>
-<?php include $_SERVER['DOCUMENT_ROOT'] . "/contactus.php";?>
-<?php include $_SERVER['DOCUMENT_ROOT'] . "/footer.php";?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/hit_tracker.php";?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Synthelize - Blog</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+  <!-- Favicons -->
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="../assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo mr-auto"><a href="../" class="scrollto">Synthelize</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="#header" class="logo mr-auto scrollto"><img src="../assets/img/logo-icon.png" alt="" class="img-fluid"></a> -->
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li><a href="../introducing-synthelize">About</a></li>
+          <li><a href="../our-process">Our Process</a></li>
+          <li><a href="../keys-to-bi-success">BI Strategy</a></li>
+          <li><a href="../data-lakes">Data Lakes</a></li>
+          <li><a href="../blog">Blog</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+
+<main id="main">
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="../">Home</a></li>
+          <li><a href="../blog">Blog</a></li>
+        </ol>
+      </div>
+    </section><!-- End Breadcrumbs -->
+<!-- ======= Blog Section ======= -->
+<section id="blog" class="blog">
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <article class="entry">
+
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
+          </div>
+
+          <h2 class="entry-title">
+            <a href="/data-lake-azure">Introducing Synthelize</a>
+          </h2>
+          <div class="entry-content">
+            <p>Who we are, what we do, and why you should work with us for all your business data needs.</p>
+            <div class="read-more">
+              <a href="/introducing-synthelize">Read More</a>
+            </div>
+          </div>
+
+        </article><!-- End blog entry -->
+      </div>
+
+      <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <article class="entry">
+
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
+          </div>
+
+          <h2 class="entry-title">
+            <a href="/data-lake-azure">Do You Have a Data Strategy?</a>
+          </h2>
+          <div class="entry-content">
+            <p>Learn the difference between having a data system and a data strategy.</p>
+            <div class="read-more">
+              <a href="/do-you-have-data-strategy/">Read More</a>
+            </div>
+          </div>
+
+        </article><!-- End blog entry -->
+      </div>
+      <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <article class="entry">
+
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
+          </div>
+
+          <h2 class="entry-title">
+            <a href="/data-lake-azure">Our Five Step Process</a>
+          </h2>
+          <div class="entry-content">
+            <p>The process we use to plan and build data systems, architectures, and visualization dashboards.</p>
+            <div class="read-more">
+              <a href="/our-process/">Read More</a>
+            </div>
+          </div>
+
+        </article><!-- End blog entry -->
+      </div>
+      <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <article class="entry">
+
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
+          </div>
+
+          <h2 class="entry-title">
+            <a href="/data-lake-azure">What is a Data Lake?</a>
+          </h2>
+          <div class="entry-content">
+            <p>Introduction to data lakes and techniques for handling big data.</p>
+            <div class="read-more">
+              <a href="/what-is-a-data-lake/">Read More</a>
+            </div>
+          </div>
+
+        </article><!-- End blog entry -->
+      </div>
+      <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <article class="entry">
+
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
+          </div>
+
+          <h2 class="entry-title">
+            <a href="/data-lake-azure">Building a Data Lake</a>
+          </h2>
+          <div class="entry-content">
+            <p>Learn the basics of building a data lake.</p>
+            <div class="read-more">
+              <a href="/building-a-data-lake">Read More</a>
+            </div>
+          </div>
+
+        </article><!-- End blog entry -->
+      </div>
+      <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <article class="entry">
+
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
+          </div>
+
+          <h2 class="entry-title">
+            <a href="/data-lake-azure">Data Lake Solutions</a>
+          </h2>
+          <div class="entry-content">
+            <p>An exploration of the major cloud vendors and their data lake solutions.</p>
+            <div class="read-more">
+              <a href="/data-lake-cloud-options/">Read More</a>
+            </div>
+          </div>
+
+        </article><!-- End blog entry -->
+      </div>
+
+      <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <article class="entry">
+
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
+          </div>
+
+          <h2 class="entry-title">
+            <a href="/data-lake-azure">Data Lake vs. Data Warehouse</a>
+          </h2>
+          <div class="entry-content">
+            <p>An examination of the roles of data lakes and data warehouses. </p>
+            <div class="read-more">
+              <a href="/data-lake-vs-data-warehouse/">Read More</a>
+            </div>
+          </div>
+
+        </article><!-- End blog entry -->
+      </div>
+
+      <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <article class="entry">
+
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
+          </div>
+
+          <h2 class="entry-title">
+            <a href="/data-lake-azure">Azure Data Lakes</a>
+          </h2>
+          <div class="entry-content">
+            <p>A closer look at Microsoft Azure data lake technology. </p>
+            <div class="read-more">
+              <a href="/data-lake-azure">Read More</a>
+            </div>
+          </div>
+
+        </article><!-- End blog entry -->
+      </div>
+
+      <div class="col-lg-4  col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+        <article class="entry">
+
+        <div class="entry-img">
+            <img src="../assets/img/blog-1.jpg" alt="" class="img-fluid">
+          </div>
+
+          <h2 class="entry-title">
+            <a href="/data-lake-azure">Keys to BI Success</a>
+          </h2>
+          <div class="entry-content">
+            <p>Five key steps to ensure the success of your next business intelligence project.</p>
+            <div class="read-more">
+              <a href="/keys-to-bi-success/">Read More</a>
+            </div>
+          </div>
+
+        </article><!-- End blog entry -->
+      </div>
+    </div>
+  </div>
+</section><!-- End Blog Section -->
+<!-- ======= Cta Section ======= -->
+<section id="cta" class="cta">
+    <div class="container">
+  
+      <div class="row" data-aos="zoom-in">
+        <div class="col-lg-9 text-center text-lg-left">
+          <h3>Let's work together!</h3>
+          <p>Every organization has data challenges that prevent it from reaching its full potential.  We have decades of experience helping organizations formulate and implement long-term data strategies.  </p>
+        </div>
+  <!--      <div class="col-lg-3 cta-btn-container text-center">
+          <a class="cta-btn align-middle" href="#contact">Contact Us</a>
+        </div> -->
+      </div>
+  
+    </div>
+  </section><!-- End Cta Section -->
+  <!-- ======= Contact Section ======= -->
+  <section id="contact" class="contact">
+    <div class="container" data-aos="fade-up">
+  
+      <div class="section-title">
+        <h2>Contact Us</h2>
+      </div>
+  
+      <div class="row mt-1 d-flex justify-content-end" data-aos="fade-right" data-aos-delay="100">
+  
+        <div class="col-lg-5">
+  <!--        <div class="info">
+            <div class="address">
+              <i class="icofont-google-map"></i>
+              <h4>Location:</h4>
+              <p>A108 Adam Street, New York, NY 535022</p>
+            </div>
+  
+            <div class="email">
+              <i class="icofont-envelope"></i>
+              <h4>Email:</h4>
+              <p>info@example.com</p>
+            </div>
+  
+            <div class="phone">
+              <i class="icofont-phone"></i>
+              <h4>Call:</h4>
+              <p>+1 5589 55488 55s</p>
+            </div>
+  
+          </div>
+  -->
+        </div>
+  
+        <div class="col-lg-6 mt-5 mt-lg-0" data-aos="fade-left" data-aos-delay="100">
+  
+          <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+            <div class="form-row">
+              <div class="col-md-6 form-group">
+                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                <div class="validate"></div>
+              </div>
+              <div class="col-md-6 form-group">
+                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                <div class="validate"></div>
+              </div>
+            </div>
+            <div class="form-group">
+              <textarea class="form-control" name="message" rows="10" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+              <div class="validate"></div>
+            </div>
+            <div class="mb-3">
+              <div class="loading">Loading</div>
+              <div class="error-message"></div>
+              <div class="sent-message">Your message has been sent. Thank you!</div>
+            </div>
+            <div class="text-center"><button type="submit">Send Message</button></div>
+          </form>
+  
+        </div>
+  
+      </div>
+  
+    </div>
+  </section><!-- End Contact Section -->
+  </main><!-- End #main -->
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+  
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 footer-info">
+              <h3>About Synthelize</h3>
+              <p>Providing data integration, data visualization, and data strategy to organizations worldwide. </p>
+            </div>
+  
+            <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> Data Strategy Formulation</li>
+              <li><i class="bx bx-chevron-right"></i> Data Integration</li>
+              <li><i class="bx bx-chevron-right"></i> BI Tools and Dashboards</li>
+              <li><i class="bx bx-chevron-right"></i> Data Lake Development</li>
+              <li><i class="bx bx-chevron-right"></i> Cloud Data Management</li>
+            </ul>
+          </div>
+  
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Key Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="../">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="../introducing-synthelize">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="../our-process">Our Process</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="../what-is-a-data-lake">What is a data lake?</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="../keys-to-bi-success">Keys to BI Success</a></li>
+            </ul>
+          </div>
+  
+  
+  <!--      <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Contact Us</h4>
+            <p>
+              A108 Adam Street <br>
+              New York, NY 535022<br>
+              United States <br><br>
+              <strong>Phone:</strong> +1 5589 55488 55<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+  
+          </div>
+  -->
+  
+        </div>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+  <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+    <!-- <div id="preloader"></div> -->
+  
+    <!-- Vendor JS Files -->
+    <script src="../../assets/vendor/jquery/jquery.min.js"></script>
+    <script src="../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../../assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="../../assets/vendor/php-email-form/validate.js"></script>
+    <script src="../../assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="../../assets/vendor/venobox/venobox.min.js"></script>
+    <script src="../../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="../../assets/vendor/aos/aos.js"></script>
+  
+    <!-- Template Main JS File -->
+    <script src="../../assets/js/main.js"></script>
+    
+  </body>
+  
+  </html>
