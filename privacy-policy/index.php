61f0e6f3175efd9390816cd274dae41e00ff867c
tbasalla <tbasalla@gmail.com>
2020-05-23 10:09:45 -0400

    automated commit

---
+++ privacy-policy/index.php
@@ -0,0 +1,9 @@
+<?php 
+  $page_title = 'Privacy Policy - Synthelize'; 
+  $meta_description = 'Our privacy policy';
+?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/hit_tracker.php";?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/header.php";?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/blog_header.php";?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/snippets/" . "privacy_policy.php";?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/footer.php";?>
---
+++ snippets/privacy_policy.php
@@ -0,0 +1,40 @@
+<h2>Privacy Policy</h2>
+<p>This privacy policy ("policy") will help you understand how Synthelize ("us", "we", "our") uses and protects the data you provide to us when you visit and use our website.</p>
+<p>We reserve the right to change this policy at any given time, of which you will be promptly updated. If you want to make sure that you are up to date with the latest changes, we advise you to frequently visit this page.</p>
+<h3>What User Data We Collect</h3>
+
+<p>When you visit the website, we may collect the following data:</p>
+<ul>
+    <li>Your IP address.</li>
+    <li>Your contact information and email address.</li>
+    <li>Other information such as interests and preferences.</li>
+    <li>Data profile regarding your online behavior on our website.</li>
+</ul>
+<h3>Why We Collect Your Data</h3>
+
+<p>We are collecting your data for several reasons:</p>
+<ul>
+    <li>To better understand your needs.</li>
+    <li>To improve our services and products.</li>
+    <li>To send you promotional emails containing the information we think you will find interesting.</li>
+    <li>To customize our website according to your online behavior and personal preferences.</li>
+</ul>
+<h3>Safeguarding and Securing the Data</h3>
+
+<p>Synthelize is committed to securing your data and keeping it confidential. Synthelize has done all in its power to prevent data theft, unauthorized access, and disclosure by implementing the latest technologies and software, which help us safeguard all the information we collect online.</p>
+
+<h3>Our Cookie Policy</h3>
+
+<p>The data we collect by using cookies is used to customize our website to your needs. After we use the data for statistical analysis, the data is completely removed from our systems.</p>
+
+<p>Please note that cookies don't allow us to gain control of your computer in any way. They are strictly used to monitor which pages you find useful and which you do not so that we can provide a better experience for you.</p>
+
+<p>If you want to disable cookies, you can do it by accessing the settings of your internet browser. (Provide links for cookie settings for major internet browsers).</p>
+
+<h3>Links to Other Websites</h3>
+
+<p>Our website contains links that lead to other websites. If you click on these links Synthelize is not held responsible for your data and privacy protection. Visiting those websites is not governed by this privacy policy agreement. Make sure to read the privacy policy documentation of the website you go to from our website.</p>
+
+<h3>Restricting the Collection of your Personal Data</h3>
+<p>The only personal information this website collects is through its message contact form.  If you do not wish Synthelize to have your personal information simply refrain from providing that information in our contact form.</p>
+<p>Synthelize will not lease, sell or distribute your personal information to any third parties, unless we have your permission. We might do so if the law forces us. Your personal information will be used when we need to send you promotional materials if you agree to this privacy policy.</p>
