da94c4ebcb8216c45d5ba98a8333664c24780ce1
tbasalla <tbasalla@gmail.com>
2020-05-23 10:12:11 -0400

    automated commit

--- privacy-policy/index.php
+++ privacy-policy/index.php
@@ -4,6 +4,5 @@
 ?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/hit_tracker.php";?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/header.php";?>
-<?php include $_SERVER['DOCUMENT_ROOT'] . "/blog_header.php";?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/snippets/" . "privacy_policy.php";?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/footer.php";?>
