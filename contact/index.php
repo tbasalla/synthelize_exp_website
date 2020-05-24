a8ff0122316379441ed154719e93c9ad23a59122
tbasalla <tbasalla@gmail.com>
2020-05-24 10:52:55 -0400

    automated commit

--- contact/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-  $page_title = 'Contact - Synthelize'; 
-  $meta_description = '';
-?>
-<?php include $_SERVER['DOCUMENT_ROOT'] . "/hit_tracker.php";?>
-<?php include $_SERVER['DOCUMENT_ROOT'] . "/header.php";?>
-<?php include $_SERVER['DOCUMENT_ROOT'] . "/snippets/" . "contact.php";?>
-<?php include $_SERVER['DOCUMENT_ROOT'] . "/footer.php";?>
