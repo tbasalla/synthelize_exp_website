ebb04254aff74670e419d997d4e01516e2f253f7
tbasalla <tbasalla@gmail.com>
2020-05-24 10:17:48 -0400

    automated commit

--- snippets/our_process.php
+++ snippets/our_process.php
@@ -6,25 +6,25 @@
                 <img src="../assets/img/our-five-step-process.jpg" alt="" class="img-fluid">
               </div>
 
-              <h2 class="entry-title">Our Data Warehouse Consulting Process</h2>
+              <h3 class="entry-title">Our Data Warehouse Consulting Process</h3>
 
               <div class="entry-content">
-				<h2>STEP 1 - Identify The Goal</h2>
+				<h3>STEP 1 - Identify The Goal</h3>
 		<p>We believe that every good initiative starts with defining the end goal. It is true that organizations have to deal with day to day challenges all the time and most activity in an organization is tactical, rather than strategic. However, organizations that want to move ahead and develop better processes need to take dedicated time to determine <strong>what</strong> they want to get out of their data.</p>
 		<p>What are your key, pressing questions regarding your company that could best be answered by data? Are those questions related to profitability? Are they related to effectiveness of sales and marketing? Are they related to production, quality assurance and customer satisfaction? Are they related to human resources and employee satisfaction? Whatever your questions we can help you to determine the right data to answer the question and the right dashboard and visualizations to keep answering the questions for years to come.</p>
-				<h2>STEP 2 - Evaluate Existing Data Systems</h2>
+				<h3>STEP 2 - Evaluate Existing Data Systems</h3>
 			<p>Once we have a vision for where you want to go in terms of data you can take a look at where you are currently at. This involves looking at your existing data systems and determining their capabilities. In many cases we will be able to adapt your existing data systems to send key information to a centralized data warehouse that can inform your decision making. In some other cases we may work with you to identify other systems to use that might better suit your needs.</p>
 				<p>Part of this process is also identifying the "other places" where your organization is storing data. A surprising amount of data in any organization is stored in informal, ad-hoc spreadsheets and databases. Part of developing a data strategy is identifying those data stores and determining a way to make them more robust and to share the data with other parts of the company.</p>
-				<h2>STEP 3 - Implement Data Architectures</h2>
+				<h3>STEP 3 - Implement Data Architectures</h3>
 			<p>This is where we get our hands dirty. When we know where you want to go and what systems you already have in place we can begin to design an overall data architecture for your organization. Sometimes this might mean considering replacing some of the data systems that were identified in step two if those systems do not collect all the data that is needed or if integration is difficult.</p>
 			<p>In some cases this might also mean taking some of the ad-hoc spreadsheets and databases of an organization and converting them to more full-featured, available systems.</p>
 			<p>In many cases it makes sense to build a centralized repository for your data&#8212;a data warehouse or data mart that gets loaded regularly with your key data elements. The design of such a system and its key parameters are driven by your vision and end goal.</p>
 			<p>Once we have a design for a data warehouse and have evaluated your systems, it's time to build the highways to bring data into your warehouse. This often involves creating batch jobs to run on a regular basis to access the underlying APIs of your systems, extract and verify data, and load it into the data warehouse. In many cases other calculations are run to summarize or reconfigure data to be in a format that best supports your reporting requirements.</p>
-					<h2>STEP 4 - Populate Visualizations and Dashboards</h2>
+					<h3>STEP 4 - Populate Visualizations and Dashboards</h3>
 			<p>Having the data in one place enables us to begin to build out the dashboards and visualizations that you need to be able to answer your key questions. Our key philosophy is that data visualizations need to be simple enough that at a glance you can get the answer to the question you want, but rich enough that you can drill down to find more data to answer the question in a fuller way. For example, do you want to know if a particular initiative is on schedule? A simple green/yellow/red indicator can help you to answer the question. If you want more data beyond that you should be able to click on the item to see the components of data that make up the determination of on track or not.</p>
 			<p>During this phase we will consider the types of reporting systems that might best suit your needs. In some cases we may be able to go with an existing software package such as Tableau. In other cases we may suggest that our team of developers code the types of visualizations that you need by hand. Your dashboards are driven by the complexity you need in terms of data display.</p>
 					<p></p>
-			<h2>STEP 5 - Envision the Future</h2>
+			<h3>STEP 5 - Envision the Future</h3>
 		<p>Over the course of working with your new data dashboard you will determine other things that you need&#8212;perhaps some of your data analysis will raise other, related questions or perhaps your organization will grow and new concerns will come to the forefront. We are not the type of organization that leaves a project without support or future effort&#8212;we believe in maintaining relationships over the years and seek to help you to adapt and extend your systems to future anticipated&#8212;and unanticipated&#8212;needs.</p>
 		<p>We hope this outline of our process helps you understand our philosophy of developing data strategy. We want you to know that we work with companies all over the spectrum of the five steps&#8212;we are more than willing to engage with you on the whole process, or on a specific step depending on where you are at in your process.</p>
 		</div>
